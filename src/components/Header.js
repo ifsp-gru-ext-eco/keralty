68025b72bc2d241bfb46de2f8b04f51bfed16570
JoaoWSL <joaoruffino2005@gmail.com>
2025-03-10 20:23:16 -0300

    centralize header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,34 @@
 import { Link } from "react-router-dom";
 
 function Header() {
-  return (
-    <div className="fixed top-0 left-0 w-full bg-white z-10 shadow-md">
-      <div className="container hidden lg:block">
-        <div className="flex justify-center items-center p-2">
-          <div className="flex justify-evenly font-rubik w-full max-w-lg mx-auto p-2 rounded-full items-center border-[2px] border-second">
-            <Link
-              to="/"
-              className="text-2xl hover:text-primary transition ease-in-out text-second"
-            >
-              Home
-            </Link>
-            <Link
-              to="/"
-              className="text-2xl hover:text-primary transition ease-in-out text-second"
-            >
-              Sobre nós
-            </Link>
-            <Link
-              to="/"
-              className="text-2xl hover:text-primary transition ease-in-out text-second"
-            >
-              Contatos
-            </Link>
-          </div>
+    return (
+        <div className="fixed top-0 left-0 w-full bg-white z-10 shadow-md flex justify-center">
+            <div className="container hidden lg:block">
+                <div className="flex justify-center items-center p-2">
+                    <div className="flex justify-evenly font-rubik w-full max-w-lg mx-auto p-2 rounded-full items-center border-[2px] border-second">
+                        <Link
+                            to="/"
+                            className="text-2xl hover:text-primary transition ease-in-out text-second"
+                        >
+                            Home
+                        </Link>
+                        <Link
+                            to="/"
+                            className="text-2xl hover:text-primary transition ease-in-out text-second"
+                        >
+                            Sobre nós
+                        </Link>
+                        <Link
+                            to="/"
+                            className="text-2xl hover:text-primary transition ease-in-out text-second"
+                        >
+                            Contatos
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Header;

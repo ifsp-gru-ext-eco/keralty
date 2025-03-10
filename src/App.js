3965151234cc3a1a5b8ae56b87ceeac42363b249
JoaoWSL <joaoruffino2005@gmail.com>
2025-03-10 20:09:04 -0300

    Cards of home

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Header from './components/Header';
 import MobHeader from './components/MobHeader.js';
 import Home from './components/Home';
 import Footer from './components/Footer.js'
+import Cards from './components/Cards.js'
 function App() {
   return (
     <Router>
       <Header />
       <MobHeader />
       <Home />
+      <Cards />
       <Footer />
     </Router>
   );

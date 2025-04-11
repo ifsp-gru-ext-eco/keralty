3c3116e46f3555b38de68720b8b16719c18e1d16
AssoDePicche <samuelprado730@gmail.com>
2025-04-11 05:51:05 -0300

    feat: add about route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './index.css';
 import Header from './components/Header';
 import MobHeader from './components/MobHeader.js';
 import Home from './components/Home';
+import AboutUs from './components/AboutUs';
 import Footer from './components/Footer.js'
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <MobHeader />
       <Routes>
         <Route path="/keralty/" element={<Home />} />
+        <Route path="/keralty/about" element={<AboutUs />} />
       </Routes>
       <Footer />
     </Router>
